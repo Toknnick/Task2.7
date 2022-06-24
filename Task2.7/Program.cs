12ea6381380375db9a155fdbf2f1b992de9aff02
Maksim <maksimus.tumannick@yandex.ru>
2022-06-24 20:47:52 +0300

    Task2.7

---
+++ Task2.7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите символ");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            Console.Clear();
+            int numberOfLine = 0;
+            int centerLinesAndRows = 2;
+            Console.SetCursorPosition(0, numberOfLine);
+
+            for (int i = 0; i <= name.Length; i++)
+            {
+                if(i == name.Length)
+                {
+                    numberOfLine++;
+                    Console.SetCursorPosition(0, numberOfLine );
+                    i -= name.Length;
+                }
+
+                if(numberOfLine >= 3) 
+                {
+                    Console.SetCursorPosition(0, numberOfLine - centerLinesAndRows);
+                    Console.Write(name);
+                    Console.SetCursorPosition(0, numberOfLine);
+                    break;
+                }
+                Console.Write(symbol);
+            }
+        }
+    }
+ }
